e6f577f2f78c80016457cb874fba43d7fe625a74
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-11-10 04:11:43 -0800

    committing blocking queue implementations

---
+++ Java-8/Concurrency/com/jav4u/conccurency/countdownlatch/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author dev
+ *
+ */
+package com.jav4u.conccurency.countdownlatch;
---
+++ Java-8/Concurrency/com/java4u/concurrency/arrayblockinqueue/Producer.java
@@ -0,0 +1,27 @@
+package com.java4u.concurrency.arrayblockinqueue;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Producer implements Runnable {
+	protected BlockingQueue queue = null;
+
+	public Producer(BlockingQueue queue) {
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		try {
+			queue.put("1");
+			Thread.sleep(1000);
+			queue.put("2");
+			Thread.sleep(1000);
+			queue.put("3");
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
