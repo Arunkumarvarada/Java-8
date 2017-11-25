9bb605306245956e4baa7713f4f686d9a44e9ccd
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-11-25 22:46:25 +0530

    Committing the selection Sorting code

--- DataStructures/Sorting/com/java4u/dsa/sorting/selectionsort/SelectionSort.java
+++ DataStructures/Sorting/com/java4u/dsa/sorting/selectionsort/SelectionSort.java
@@ -3,7 +3,22 @@ package com.java4u.dsa.sorting.selectionsort;
 public class SelectionSort {
 
 	public void selectionSort(int[] arr) {
+		print(arr);
+		int size = arr.length;
+		int i, j, max, temp;
+		for (i = 0; i < size - 1; i++) {
+			max = 0;
+			for (j = 1; j < size - 1 - i; j++) {
+				if (arr[j] > arr[max]) {
+					max = j;
+				}
+			}
+			temp = arr[size - 1 - i];
+			arr[size - 1 - i] = arr[max];
+			arr[max] = temp;
+		}
 
+		print(arr);
 	}
 
 	public void print(int arr[]) {
