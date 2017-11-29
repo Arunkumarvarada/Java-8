fc0d930ae1f8e27b3a0876fd3903f13998df8a67
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-11-29 21:42:11 +0530

    committing code related to stack and queue implementation using arrays

---
+++ DataStructures/Queue/com/java4u/ds/queue/QueueUsingArray.java
@@ -0,0 +1,71 @@
+package com.java4u.ds.queue;
+
+public class QueueUsingArray {
+
+	private int capacity;
+	private int queue[];
+	private int front = 0;
+	private int rear = -1;
+	private int currentSize = 0;
+
+	public QueueUsingArray(int size) {
+		this.capacity = size;
+		queue = new int[capacity];
+	}
+
+	public void enQueue(int data) {
+		if (isQueueFull()) {
+			System.out.println("Unable to add an Element overflow occured " + data);
+		} else {
+			rear++;
+			if (rear == capacity - 1) {
+				rear = 0;
+			}
+			queue[rear] = data;
+			currentSize++;
+			System.out.println("Element " + data + " is pushed to Queue !!");
+		}
+	}
+
+	private void deQueue() {
+		if (isQueueEmpty()) {
+			System.out.println("Underflow! unable to remove the element from Queue");
+		} else {
+			front++;
+			if (front == capacity - 1) {
+				System.out.println("pop operation done! removed: " + queue[front - 1]);
+				front = 0;
+			} else {
+				System.out.println("Pop operation done! removed: " + queue[front - 1]);
+			}
+			currentSize--;
+		}
+	}
+
+	public boolean isQueueEmpty() {
+		return (currentSize == 0) ? true : false;
+	}
+
+	public boolean isQueueFull() {
+		return (currentSize == capacity) ? true : false;
+	}
+
+	public static void main(String[] args) {
+		QueueUsingArray queue = new QueueUsingArray(12);
+		queue.enQueue(4);
+		queue.deQueue();
+		queue.enQueue(56);
+		queue.enQueue(2);
+		queue.enQueue(67);
+		queue.deQueue();
+		queue.deQueue();
+		queue.enQueue(24);
+		queue.deQueue();
+		queue.enQueue(98);
+		queue.enQueue(45);
+		queue.enQueue(23);
+		queue.enQueue(435);
+
+	}
+
+}
---
+++ DataStructures/Stack/com/java4u/ds/stack/StackUsingArray.java
@@ -0,0 +1,84 @@
+package com.java4u.ds.stack;
+
+public class StackUsingArray {
+
+	private int MAX_SIZE = 10;
+	private int stack[];
+	private int top;
+
+	public StackUsingArray(int size) {
+		this.MAX_SIZE = size;
+		stack = new int[MAX_SIZE];
+		top = -1;
+	}
+
+	public void print() {
+		if (isEmpty()) {
+			System.out.println("No Elements are present in the stack!!");
+		}
+		for (int i : stack) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+
+	public void push(int data) throws Exception {
+		if (isStackFull()) {
+			throw new Exception("Stack is Full");
+		}
+		stack[++top] = data;
+	}
+
+	public int peek() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Stack is Empty");
+		}
+		return stack[top];
+	}
+
+	public int pop() throws Exception {
+		if (isEmpty()) {
+			throw new Exception("Stack is Empty");
+		}
+		return stack[top--];
+	}
+
+	public boolean isEmpty() {
+		return (top == -1) ? true : false;
+	}
+
+	public boolean isStackFull() {
+		return (top == MAX_SIZE - 1);
+	}
+
+	public int getsize() {
+		return top + 1;
+	}
+
+	public static void main(String[] args) {
+		StackUsingArray stack = new StackUsingArray(10);
+		try {
+			System.out.println(stack.isEmpty());
+			stack.push(23);
+			stack.push(22);
+			stack.push(24);
+			stack.push(25);
+			stack.push(27);
+			stack.push(28);
+			stack.push(29);
+			stack.push(20);
+			stack.push(21);
+			stack.push(31);
+			stack.print();
+			System.out.println(stack.getsize());
+			System.out.println(stack.isStackFull());
+			// stack.push(33);
+			System.out.println(stack.pop());
+			System.out.println(stack.pop());
+			System.out.println(stack.peek());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
