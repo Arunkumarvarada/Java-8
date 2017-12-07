8531d47220884e2ef9fdb9d21e360fa9fdf5b12b
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-12-07 22:52:37 +0530

    Added problem asked in intel hackerrank

--- DataStructures/Arrays/com/java4u/ds/arrays/DistinctCountofPairSum.java
+++ DataStructures/Arrays/com/java4u/ds/arrays/DistinctCountofPairSum.java
@@ -1,5 +1,28 @@
 package com.java4u.ds.arrays;
 
-public class DistinctCountofPairSum {
+import java.util.HashMap;
+import java.util.Map;
 
+public class DistinctCountofPairSum {
+	static int numberOfPairs(int[] a, long k) {
+		int count = 0;
+		Map<Integer, Integer> pairs = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < a.length; i++) {
+
+			if (pairs.containsKey(a[i])) {
+
+				count++;
+			} else
+				pairs.put((int) (k - a[i]), a[i]);
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+
+		int[] a = { 2, 45, 7, 3, 5, 1, 8, 9 };
+		System.out.println(numberOfPairs(a, 10));
+
+	}
 }
