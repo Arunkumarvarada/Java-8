6dc0fa8df4941bfc662bf58d9d86249dcd224361
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-12-19 10:02:28 +0530

    Committed the Sorting elements based on the frequency

---
+++ DataStructures/Arrays/com/java4u/ds/arrays/SumOfNumbers.java
@@ -0,0 +1,29 @@
+package com.java4u.ds.arrays;
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = in.nextInt();
+		}
+
+		System.out.println(calculateArraySum(arr));
+		in.close();
+
+	}
+
+	private static long calculateArraySum(int[] arr) {
+		long result = 0;
+		for (int a : arr) {
+			result += a;
+		}
+		return result;
+	}
+
+}
