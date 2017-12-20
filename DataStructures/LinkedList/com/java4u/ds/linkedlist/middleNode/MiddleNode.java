e8f5d7ed8c1bd4838724939679dce7a57048b306
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-12-20 17:10:44 +0530

    updating the file

--- DataStructures/LinkedList/com/java4u/ds/linkedlist/middleNode/MiddleNode.java
+++ DataStructures/LinkedList/com/java4u/ds/linkedlist/middleNode/MiddleNode.java
@@ -1,9 +1,38 @@
 package com.java4u.ds.linkedlist.middleNode;
 
+import com.java4u.ds.linkedlist.Node;
+
 public class MiddleNode {
 
+	public static Node findMiddleNode(Node current) {
+		if (current == null) {
+			return current;
+		}
+		if (current.getNext() == null) {
+			return current;
+		}
+		Node slow = current;
+		Node fast = current;
+
+		while (fast != null && fast.getNext() != null) {
+			fast = fast.getNext().getNext();
+			slow = slow.getNext();
+		}
+		return slow;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Node head = new Node(1);
+		Node node1 = new Node(2);
+		Node node2 = new Node(3);
+		Node node3 = new Node(4);
+		Node node4 = new Node(5);
+		head.setNext(node1);
+		node1.setNext(node2);
+		node2.setNext(node3);
+		node3.setNext(node4);
+		Node middleNode = findMiddleNode(head);
+		System.out.println(middleNode.getData());
 
 	}
 
