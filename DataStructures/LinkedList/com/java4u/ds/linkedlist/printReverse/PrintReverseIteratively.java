4ab7fe72f9eb38b219819493f053befc67413b86
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-12-20 17:09:39 +0530

    find middle node in linked list

--- DataStructures/LinkedList/com/java4u/ds/linkedlist/printReverse/PrintReverseIteratively.java
+++ DataStructures/LinkedList/com/java4u/ds/linkedlist/printReverse/PrintReverseIteratively.java
@@ -3,6 +3,9 @@ package com.java4u.ds.linkedlist.printReverse;
 import com.java4u.ds.linkedlist.Node;
 
 public class PrintReverseIteratively {
+	public void print(Node current) {
+		
+	}
 	public void printReverse(Node current) {
 		
 		
