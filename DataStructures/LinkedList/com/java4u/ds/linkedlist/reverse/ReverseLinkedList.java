1a06dd6b446395410f0a51434422f2aa1decfa74
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-12-20 13:52:09 +0530

    Reversing Linked List Using Recursion

--- DataStructures/LinkedList/com/java4u/ds/linkedlist/reverse/ReverseLinkedList.java
+++ DataStructures/LinkedList/com/java4u/ds/linkedlist/reverse/ReverseLinkedList.java
@@ -1,9 +1,73 @@
 package com.java4u.ds.linkedlist.reverse;
 
+import com.java4u.ds.linkedlist.Node;
+
 public class ReverseLinkedList {
 
+	private Node head;
+
+	public Node getHead() {
+		return head;
+	}
+
+	public void setHead(Node head) {
+		this.head = head;
+	}
+
+	public void reverseLinkedListRecursive() {
+		reverseLinkedListRecursive(head);
+	}
+
+	public void reverseLinkedListRecursive(Node current) {
+
+		if (current == null) {
+			return;
+		}
+		if (current.getNext() == null) {
+			this.head = current;
+			return;
+		}
+		reverseLinkedListRecursive(current.getNext());
+		current.getNext().setNext(current);
+		current.setNext(null);
+
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ReverseLinkedList list = new ReverseLinkedList();
+		list.createTestList(5);
+		list.printlist();
+		list.reverseLinkedListRecursive();
+		list.printlist();
+
+	}
+
+	private void printlist() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.getData() + " -> ");
+			temp = temp.getNext();
+		}
+		System.out.println("X");
+
+	}
+
+	private void createTestList(int n) {
+		if (n < 1)
+			return;
+		int i = 1;
+		Node temp = null;
+		while (i <= n) {
+			Node node = new Node(i);
+			if (head == null) {
+				head = node;
+				temp = head;
+			} else {
+				temp.setNext(node);
+				temp = node;
+			}
+			i++;
+		}
 
 	}
 
