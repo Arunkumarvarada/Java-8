8546d47e344e6fb381bc4aa5ff67973f9825dafd
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2017-12-24 20:49:20 +0530

    committed code related to NthNode from the End of Linked list

--- DataStructures/LinkedList/com/java4u/ds/linkedlist/nthNode/NthNodeFromLast.java
+++ DataStructures/LinkedList/com/java4u/ds/linkedlist/nthNode/NthNodeFromLast.java
@@ -1,11 +1,51 @@
 package com.java4u.ds.linkedlist.nthNode;
 
-public class NthNodeFromLast {
+import com.java4u.ds.linkedlist.Node;
 
-	
-	public static void main(String[] args) {
-		
+public class NthNodeFromLast {
+	public Node NthNodeFromEnd(Node head, int n) {
+		Node temp = head;
+		Node pthNode = null;
+		for (int i = 0; i < n; i++) {
+			if (temp != null) {
+				temp = temp.getNext();
+			}
+		}
+		while (temp != null) {
+			if (pthNode != null) {
+				pthNode = head;
+			} else {
+				pthNode = pthNode.getNext();
+			}
+			temp = temp.getNext();
+		}
+
+		if (pthNode != null) {
+			return pthNode;
+		} else {
+			return null;
+		}
 
 	}
 
+	public static void main(String[] args) {
+		NthNodeFromLast list = new NthNodeFromLast();
+
+		Node head = new Node(1);
+		Node n1 = new Node(2);
+		Node n2 = new Node(3);
+		Node n3 = new Node(4);
+		Node n4 = new Node(5);
+		Node n5 = new Node(6);
+		head.setNext(n1);
+		n1.setNext(n2);
+		n2.setNext(n3);
+		n3.setNext(n4);
+		n4.setNext(n5);
+		Node node = list.NthNodeFromEnd(head, 2);
+		if (node != null) {
+			System.out.println(node.getData());
+		}
+
+	}
 }
--- DataStructures/LinkedList/com/java4u/ds/linkedlist/nthNode/NthNodeFromLastUsingRecursion.java
+++ DataStructures/LinkedList/com/java4u/ds/linkedlist/nthNode/NthNodeFromLastUsingRecursion.java
@@ -1,10 +1,55 @@
 package com.java4u.ds.linkedlist.nthNode;
 
+import com.java4u.ds.linkedlist.Node;
+
 public class NthNodeFromLastUsingRecursion {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int count = 0;
 
+	public int nthNodeFromEnd(Node head, int nodeFromLast) {
+
+		if (nodeFromLast <= 0 || head == null) {
+			return -1;
+		}
+
+		Node ptrA = head, ptrB = head;
+
+		// Moving ptrB N nodes from start keeping ptrA at head node.
+		for (int i = 0; i < nodeFromLast; i++) {
+
+			// If ptrB reaches NULL, then return -1 as indication that given Nth value is
+			// incorrect and list doesn't contain N elements.
+			if (ptrB == null) {
+				return -1;
+			}
+			ptrB = ptrB.getNext();
+		}
+
+		// Now incrementing ptrA and ptrB one node at a time until ptrB encounters NULL,
+		// when it encounters NULL then ptrA will be at Nth position from end of list.
+		while (ptrB != null) {
+			ptrB = ptrB.getNext();
+			ptrA = ptrA.getNext();
+		}
+
+		return ptrA.getData();
+
+	}
+
+	public static void main(String[] args) {
+		NthNodeFromLastUsingRecursion list = new NthNodeFromLastUsingRecursion();
+		Node head = new Node(1);
+		Node n1 = new Node(2);
+		Node n2 = new Node(3);
+		Node n3 = new Node(4);
+		Node n4 = new Node(5);
+		Node n5 = new Node(6);
+		head.setNext(n1);
+		n1.setNext(n2);
+		n2.setNext(n3);
+		n3.setNext(n4);
+		n4.setNext(n5);
+		System.out.println(list.nthNodeFromEnd(head, 2));
 	}
 
 }
