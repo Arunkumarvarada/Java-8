7fe5e8912ab48f0f0cd50bcca6f48a9636303a29
Arun Kumar Varada Gopal <arunkumar.vg@hotmail.com>
2018-03-27 13:42:08 +0530

    formatted the code

--- JavaMultiThreading/CodingExamples/com/java4u/multithreading/sempahore/MutexTest.java
+++ JavaMultiThreading/CodingExamples/com/java4u/multithreading/sempahore/MutexTest.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Semaphore;
 
 public class MutexTest {
 
-	static Semaphore semaphore = new Semaphore(10);
+	static Semaphore semaphore = new Semaphore(1);
 
 	static class MyLockerThread extends Thread {
 
@@ -15,42 +15,27 @@ public class MutexTest {
 		}
 
 		public void run() {
-
 			try {
-
 				System.out.println(name + " : acquiring lock...");
 				System.out.println(name + " : available Semaphore permits now: " + semaphore.availablePermits());
-
 				semaphore.acquire();
 				System.out.println(name + " : got the permit!");
-
 				try {
-
 					for (int i = 1; i <= 5; i++) {
-
 						System.out.println(name + " : is performing operation " + i + ", available Semaphore permits : "
 								+ semaphore.availablePermits());
-
 						// sleep 1 second
 						Thread.sleep(1000);
-
 					}
-
 				} finally {
-
 					// calling release() after a successful acquire()
 					System.out.println(name + " : releasing lock...");
 					semaphore.release();
 					System.out.println(name + " : available Semaphore permits now: " + semaphore.availablePermits());
-
 				}
-
 			} catch (InterruptedException e) {
-
 				e.printStackTrace();
-
 			}
-
 		}
 
 	}
--- JavaMultiThreading/CodingExamples/com/java4u/multithreading/sempahore/SemaphoreTest.java
+++ JavaMultiThreading/CodingExamples/com/java4u/multithreading/sempahore/SemaphoreTest.java
@@ -5,10 +5,9 @@ import java.util.concurrent.Semaphore;
 public class SemaphoreTest {
 
 	// max 1 people
-	static Semaphore semaphore = new Semaphore(1);
+	static Semaphore semaphore = new Semaphore(10);
 
 	static class MyLockerThread extends Thread {
-
 		String name = "";
 
 		MyLockerThread(String name) {
@@ -16,44 +15,28 @@ public class SemaphoreTest {
 		}
 
 		public void run() {
-
 			try {
-
 				System.out.println(name + " : acquiring lock...");
 				System.out.println(name + " : available Semaphore permits now: " + semaphore.availablePermits());
-
 				semaphore.acquire();
 				System.out.println(name + " : got the permit!");
-
 				try {
-
 					for (int i = 1; i <= 5; i++) {
-
 						System.out.println(name + " : is performing operation " + i + ", available Semaphore permits : "
 								+ semaphore.availablePermits());
-
 						// sleep 1 second
 						Thread.sleep(1000);
-
 					}
-
 				} finally {
-
 					// calling release() after a successful acquire()
 					System.out.println(name + " : releasing lock...");
 					semaphore.release();
 					System.out.println(name + " : available Semaphore permits now: " + semaphore.availablePermits());
-
 				}
-
 			} catch (InterruptedException e) {
-
 				e.printStackTrace();
-
 			}
-
 		}
-
 	}
 
 	public static void main(String[] args) {
